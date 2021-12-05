0ca7daaf53f8421648c454fbadc266d8bc2a2356
Strahinja <strahinja.jkc@gmail.com>
2021-12-05 15:35:49 +0100

    Initial Commit

---
+++ stringOperations/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  stringOperations
+//
+//  Created by Strahinja Jokic on 5.12.21..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
